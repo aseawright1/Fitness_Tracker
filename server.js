7fd8ab66581d9af2cc752599e5a44186af314db1
Alek <56727628+aseawright1@users.noreply.github.com>
2020-04-25 09:56:25 -0500

    update server.js for mongo db

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://username:password1@ds161505.mlab.com:61505/heroku_k44j263q',
 { useNewUrlParser: true });
 
 // routes
